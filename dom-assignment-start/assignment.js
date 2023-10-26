decba1ca918e41fbce6e529f1b05f682a9ad95aa
Oksana Melnyk <oks.melnuk@gmail.com>
2023-10-26 08:57:57 +0200

    solved

---
+++ dom-assignment-start/assignment.js
@@ -0,0 +1,8 @@
+const task1 = document.querySelector('#task-1');
+task1.style.backgroundColor='black';
+task1.style.color='#fff';
+document.head.title = 'Assignment - Solved!'
+const head = document.querySelector('head');
+head.title = 'Assignment - Solved!2' 
+
+document.querySelector('h1').textContent = 'Assignment - Solved!';'Assignment - Solved!'
