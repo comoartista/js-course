e1bc4b616aaa5fe09b3092cc2e7ec513cf8f3c31
Oksana Melnyk <oks.melnuk@gmail.com>
2023-10-26 13:48:21 +0200

    Creating a Movie, Clearing the Input

--- dom-06-demo-project-setup/assets/scripts/app.js
+++ dom-06-demo-project-setup/assets/scripts/app.js
@@ -5,14 +5,22 @@ const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
 const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
 const userInputs = addMovieModal.querySelectorAll('input');
 
+const movies = [];
 
 const toggleBackdrop = () => {
     backdrop.classList.toggle('visible');
-}
+};
+
 const toggleMovieModal = () => {
     addMovieModal.classList.toggle('visible');
     toggleBackdrop();
-}
+};
+
+const clearMovieInput = () => {
+    for (const userInput of userInputs) {
+        userInput.value = '';
+    }
+};
 
 const cancelAddMovieHandler = ()  => {
     toggleMovieModal();
@@ -32,12 +40,21 @@ const addMovieHandler = () => {
         alert('Please enter valid values (rating between 1 and 5).');
         return;
     }
+    const newMovie = {
+        title: titleValue,
+        image: imgUrlValue,
+        ratingValue: ratingValue
+    }
 
-}
+    movies.push(newMovie);
+    console.log(movies);
+    toggleMovieModal();
+    clearMovieInput();
+};
 
 const backdropClickHandler = ()  => {
     toggleMovieModal();
-} 
+}; 
 
 startAddMovieButton.addEventListener('click', toggleMovieModal);
 backdrop.addEventListener('click', backdropClickHandler);
