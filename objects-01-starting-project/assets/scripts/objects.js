d90f73312668c4166b506264dfdba7962e0a2626
Oksana Melnyk <oks.melnuk@gmail.com>
2023-10-31 10:22:33 +0100

    add filtered

--- objects-01-starting-project/assets/scripts/objects.js
+++ objects-01-starting-project/assets/scripts/objects.js
@@ -1,8 +1,33 @@
-// const movieList = document.getElementById('movie-list');
 const addMovieBtn = document.getElementById('add-movie-btn');
 const searchBtn = document.getElementById('search-btn');
 
 const movies = [];
+
+const renderMovies = (filter = '') => {
+    const movieList = document.getElementById('movie-list');
+    if (movies.length === 0) {
+        movieList.classList.remove('visible');
+        return;
+    } else {
+        movieList.classList.add('visible');
+    }
+    movieList.innerHTML = ``
+    const filteresMovies = !filter 
+        ? movies 
+        : movies.filter(movie => movie.info.title.includes(filter));
+ 
+    filteresMovies.forEach((movie) => {
+        const movieEl = document.createElement('li');
+        let text = movie.info.title + ' - '
+        for (const key in movie.info) {
+            if (key != title) {
+                text = text + `${key}: ${movie.info[key]}`
+            }
+        }
+        movieEl.textContent = text;
+        movieList.append(movieEl);
+    })
+}
 
 const addMovieHandler = () => {
     const title = document.getElementById('title').value;
@@ -22,11 +47,18 @@ const addMovieHandler = () => {
             title,
             [extraName]: extraValue,
         },
-        id: Math.random()
+        id: Math.random().toString()
     }
 
     movies.push(newMovie);
-    console.log(movies);
+    renderMovies();
+}
+
+const searchMovieHandler = () => {
+    const filterTerm = document.getElementById('filter-title').value;
+    renderMovies(filterTerm);
+
 }
 
 addMovieBtn.addEventListener('click', addMovieHandler);
+searchBtn.addEventListener('click', searchMovieHandler);
